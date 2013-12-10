ff5db158db925690b0d2362edc21b59178493db2
apetri <apetri@phys.columbia.edu>
2013-12-10 14:34:34 -0500

    Fixed typos in BGQ submission scripts

--- localStorage/ics/mQ2-series/data_Gadget/Jobs/jobsubmitQ.sh
+++ localStorage/ics/mQ2-series/data_Gadget/Jobs/jobsubmitQ.sh
@@ -18,13 +18,27 @@ NUM_MPI_TASKS=1024
 LOGSDIR=/bgsys/home3/a/apetri/IG_Pipeline_0.1/localStorage/ics/mQ2-series/data_Gadget/Logs
 
 G_ROOT=/bgsys/home3/a/apetri/IG_Pipeline_0.1/localStorage/ics/mQ2-series/data_Gadget/Parameters
-G_FILE_1=$G_ROOT/m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic1.param
-G_FILE_2=$G_ROOT/m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic2.param
-G_FILE_3=$G_ROOT/m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic3.param
-G_FILE_4=$G_ROOT/m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic4.param
+G_FILE_1=m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic1.param
+G_FILE_2=m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic2.param
+G_FILE_3=m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic3.param
+G_FILE_4=m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic4.param
 
-ARGS="$NUM_SIMS $TASKS_PER_SIM $G_FILE1 $G_FILE_2 $G_FILE_3 $G_FILE_4"
+ARGS="$NUM_SIMS $TASKS_PER_SIM $G_FILE_1 $G_FILE_2 $G_FILE_3 $G_FILE_4"
 
 #execution
+COMMAND="runjob --block $BLOCKID --exe $EXECUTABLE -p $CORES_PER_NODE -np $NUM_MPI_TASKS --args $ARGS --cwd $G_ROOT > $LOGSDIR/gadget.out 2> $LOGSDIR/gadget.err"
 
-runjob --block $BLOCKID --exe $EXECUTABLE -p $CORES_PER_NORE -np $NUM_MPI_TASKS --args $ARGS --cwd $LOGSDIR > gadget.out 2> gadget.err
+echo "You will be executing this command:"
+echo ""
+echo "$COMMAND"
+echo ""
+echo "Do you wish to proceed? (y/n)"
+
+read ANSWER
+
+if [ $ANSWER == "y" ]; then
+	$COMMAND
+else
+	echo "Aborting"
+fi
+
--- localStorage/ics/mQ2-series/data_N-GenIC/Jobs/jobsubmitQ.sh
+++ localStorage/ics/mQ2-series/data_N-GenIC/Jobs/jobsubmitQ.sh
@@ -13,15 +13,29 @@ CORES_PER_NODE=2
 NUM_MPI_TASKS=256
 
 LOGSDIR=/bgsys/home3/a/apetri/IG_Pipeline_0.1/localStorage/ics/mQ2-series/data_N-GenIC/Logs
-
 IC_ROOT=/bgsys/home3/a/apetri/IG_Pipeline_0.1/localStorage/ics/mQ2-series/data_N-GenIC/Parameters
-IC_FILE_1=$IC_ROOT/ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic1.param
-IC_FILE_2=$IC_ROOT/ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic2.param
-IC_FILE_3=$IC_ROOT/ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic3.param
-IC_FILE_4=$IC_ROOT/ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic4.param
 
-ARGS="$NUM_MPI_TASKS $IC_FILE1 $IC_FILE_2 $IC_FILE_3 $IC_FILE_4"
+IC_FILE_1=ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic1.param
+IC_FILE_2=ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic2.param
+IC_FILE_3=ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic3.param
+IC_FILE_4=ics_m-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.798_ic4.param
+
+ARGS="$NUM_MPI_TASKS $IC_FILE_1 $IC_FILE_2 $IC_FILE_3 $IC_FILE_4"
 
 #execution
 
-runjob --block $BLOCKID --exe $EXECUTABLE -p $CORES_PER_NORE -np $NUM_MPI_TASKS --args $ARGS --cwd $LOGSDIR > ic.out 2> ic.err
+COMMAND="runjob --block $BLOCKID --exe $EXECUTABLE -p $CORES_PER_NODE -np $NUM_MPI_TASKS --args $ARGS --cwd $IC_ROOT > $LOGSDIR/ic.out 2> $LOGSDIR/ic.err"
+
+echo "You will be executing this command:"
+echo ""
+echo "$COMMAND"
+echo ""
+echo "Do you wish to proceed? (y/n)"
+
+read ANSWER
+
+if [ $ANSWER == "y" ]; then
+	$COMMAND
+else
+	echo "Aborting"
+fi
