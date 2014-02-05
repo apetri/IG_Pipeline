8023e4b2e87e0de8f2a381474ecaf6fbdd18b9bf
apetri <apetri@phys.columbia.edu>
2014-02-05 11:21:56 -0500

    submission.py generates camb script in the correct location

--- submission.py
+++ submission.py
@@ -1,4 +1,5 @@
-import sys,os,ConfigParser
+import sys,os,stat,ConfigParser
+import StringIO
 
 #Check if options file is provided
 if(len(sys.argv)<2):
@@ -10,4 +11,25 @@ options = ConfigParser.RawConfigParser()
 options.readfp(file(sys.argv[1],"r"))
 
 #This function generates CAMB submission script for BGQ
-print options.options("cosmologies_camb")
+def generate_BGQ_camb_submission(options):
+	
+	S = StringIO.StringIO()
+	
+	S.write("""#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <BLOCKID>" >&2
+    exit 1
+fi""")
+
+	S.seek(0)
+	return S.read()
+
+
+#Here we write the submission scripts to the appropriate folders
+
+#CAMB submission script
+camb_script_directory = "%s/%s/localStorage/ics/%s-series/data_CAMB/Jobs/"%(options.get("paths","home_path"),options.get("paths","repository_path"),options.get("series","series_name"))
+camb_script_filename = "jobsubmitQ_CAMB_%s-series.sh"%options.get("series","series_name")
+file(camb_script_directory+camb_script_filename,"w").write(generate_BGQ_camb_submission(options))
+os.chmod(camb_script_directory+camb_script_filename,stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH)
