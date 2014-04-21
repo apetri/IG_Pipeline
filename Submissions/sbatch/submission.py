ac4415689833e1f139ceaadfffd7d66b807611d3
apetri <apetri@phys.columbia.edu>
2014-04-21 11:53:00 -0400

    submission.py generates ngenic submission

--- Submissions/sbatch/submission.py
+++ Submissions/sbatch/submission.py
@@ -87,6 +87,76 @@ wait
 #######################################################
 ############N-GenIC submission script##################
 #######################################################
+def generateNgenICSubmission(options):
+
+	S = StringIO.StringIO()
+	S.write("""#!/bin/bash\n\n""")
+
+	########Submission script directives########
+
+	#Job name#
+	S.write("""
+##########################################
+#############Directives###################
+##########################################
+
+#%s -J %sN-GenIC\n
+"""%(qsys,options.get("user","username")))
+
+	#Output and error logs#
+	logPath = "%s/IG_Pipeline_0.1/localStorage/ics/%s-series/data_N-GenIC/Logs/"%(options.get("user","home"),options.get("series","series"))
+
+	S.write("""#%s -o %s%sN-GenIC.o%%j\n"""%(qsys,logPath,options.get("user","username")))
+	S.write("""#%s -e %s%sN-GenIC.e%%j\n\n"""%(qsys,logPath,options.get("user","username")))
+
+	#Computing resources#
+	cosmofile = file(options.get("ngenic","models_file"),"r")
+	cosmologies = cosmofile.readlines()
+	cosmofile.close()
+
+	S.write("""#%s -n %d\n"""%(qsys,options.getint("ngenic","cores")))
+	S.write("""#%s -p %s\n"""%(qsys,options.get("ngenic","queue")))
+	S.write("""#%s -t %s\n\n"""%(qsys,options.get("ngenic","wall_time")))
+
+	#Email notifications
+	S.write("""#%s --mail-user=%s\n"""%(qsys,options.get("user","email")))
+	S.write("""#%s --mail-type=all\n"""%qsys)
+
+	########Execution directives##########
+	S.write("""
+
+###################################################
+#################Execution#########################
+###################################################
+
+""")
+
+	parameterDir = "%s/IG_Pipeline_0.1/localStorage/ics/%s-series/data_N-GenIC/Parameters"%(options.get("user","home"),options.get("series","series"))
+	executable = "%s/IG_Pipeline_0.1/N-GenIC/%s"%(options.get("user","home"),options.get("ngenic","executable"))
+
+	S.write("""
+cd %s
+
+"""%parameterDir)
+	
+	for cosmology in cosmologies:
+
+		ngenic_args = ""
+		
+		cosmology = cosmology.strip("\n")
+		cosmology = cosmology.split(" ")
+		cosmology_id,first_ic,last_ic = cosmology[0],int(cosmology[1]),int(cosmology[2])
+
+		for i in range(first_ic,last_ic+1):
+
+			ngenic_args += "ics_%s-%db%d_%s_ic%d.param "%(options.get("series","series"),options.getint("series","particles_side"),options.getint("series","box_size_mpc"),cosmology_id,i)
+
+		S.write("""ibrun -n %d -o 0 %s %s
+"""%(options.getint("ngenic","cores"),executable,ngenic_args))
+
+	S.seek(0)
+	return S.read()
+
 
 #######################################################
 ############Gadget2 submission script##################
