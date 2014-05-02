60a8d2f4f929f110fe06ecce2fe312c92f214277
apetri <apetri@phys.columbia.edu>
2014-05-02 11:41:52 -0400

    Added file existence check to make_cosmology_file.py

--- Submissions/SLURM/make_cosmology_file.py
+++ Submissions/SLURM/make_cosmology_file.py
@@ -1,9 +1,13 @@
-import sys
+import sys,glob,ConfigParser
 import numpy as np
 
-if(len(sys.argv)<2):
-	print "Usage: python %s <file_with_cosmology_parameters>"%sys.argv[0]
+if(len(sys.argv)<3):
+	print "Usage: python %s <precambrian_cosmologies_file> <ini_options_file>"%sys.argv[0]
 	exit(1)
+
+#Parse options
+options = ConfigParser.RawConfigParser()
+options.readfp(file(sys.argv[2],"r"))
 
 cosmopar = np.loadtxt(sys.argv[1],comments="#")
 
@@ -12,11 +16,8 @@ if(len(cosmopar.shape)==1):
 else:
 	Ncosmo = cosmopar.shape[0]
 
-print "What's the filename you want for the CAMB cosmology file?"
-cambFileName = raw_input("-->")
-
-print "What's the filename you want for the N-GenIC/Gadget cosmology file?"
-gadgetFileName = raw_input("-->")
+cambFileName = options.get("camb","models_file")
+gadgetFileName = options.get("gadget","models_file")
 
 print "What's the first initial condition you want to generate? Enter an integer:"
 firstIC = int(raw_input("-->"))
@@ -45,9 +46,17 @@ cambFile = file(cambFileName,"w")
 gadgetFile = file(gadgetFileName,"w")
 
 for Id in cambIds:
+	#Throw user a warning if the Precambrian didn't generate this parameter file for some reason
+	cambParamFile = "%s/%s/localStorage/ics/%s-series/data_CAMB/Parameters/params_%s-%s.ini"%(options.get("user","home"),options.get("user","IG_Repository"),options.get("series","series"),options.get("series","series"),Id)
+	if(len(glob.glob(cambParamFile))==0) print "WARNING! The CAMB parameter file associated with %s does not exist!\n"%Id
+	
 	cambFile.write("%s\n"%Id)
 
 for Id in gadgetIds:
+	#Throw user a warning if the Precambrian didn't generate this parameter file for some reason
+	gadgetParamFile = "%s/%s/localStorage/ics/%s-series/data_Gadget/Parameters/%s-%db%d_%s"%(options.get("user","home"),options.get("user","IG_Repository"),options.get("series","series"),options.get("series","series"),options.getint("series","particles_side"),options.getint("series","box_size_mpc"),Id)
+	if(len(glob.glob(gadgetParamFile+"*"))==0) print "WARNING! The CAMB parameter file associated with %s does not exist!\n"%Id
+
 	gadgetFile.write("%s %d %d\n"%(Id,firstIC,lastIC))
 
 cambFile.close()
