4b8e3849f09e19c11839755e90d1bad15f1b0a83
apetri <apetri@phys.columbia.edu>
2014-10-23 15:37:27 -0500

    measure different snapshots in series

--- Planes/make_planes.py
+++ Planes/make_planes.py
@@ -14,62 +14,68 @@ from mpi4py import MPI
 ########################################################################
 
 #TODO options are hardcoded for now
-snapshot_path = "/scratch/02918/apetri/Storage/sims/snapshots/cfhtcov-series/cfhtcov-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.800_ic1"
+snapshot_path = "/scratch/02918/apetri/Storage/sims/snapshots/dev-series/dev-512b240_Om0.260_Ol0.740_w-1.000_ns0.960_si0.800_ic1"
 snapshot_file = "snapshot_"
 save_path = "/scratch/02918/apetri/Planes4096"
 plane_resolution = 4096
+num_snapshots = 1
 
 #########################################################################
 
 #Split the communicator to handle multiple snapshots
 comm = MPI.COMM_WORLD
-newcomm = comm.Split(color=comm.rank//16,key=comm.rank)
 
 #Initialize MPI environment
 try:
-	pool = MPIWhirlPool(comm=newcomm)
+	pool = MPIWhirlPool(comm=comm)
 except:
 	pool = None
 
-#Open the snapshot
-snap = Gadget2Snapshot.open(os.path.join(snapshot_path,snapshot_file+"{0:03d}".format(comm.rank//16)),pool=pool)
+for n in range(num_snapshots):
 
-if pool is not None:
-	print("Rank {0} reading snapshot from {1}".format(comm.rank,snap.header["files"][0]))
+	#Open the snapshot
+	snap = Gadget2Snapshot.open(os.path.join(snapshot_path,snapshot_file+"{0:03d}".format(n),pool=pool))
 
-#Get the positions of the particles
-snap.getPositions()
+	if pool is not None:
+		print("Rank {0} reading snapshot from {1}".format(comm.rank,snap.header["files"][0]))
 
-#Decide where to cut the lenses
-cut_points = np.array([40.0,120.0,200.0]) * snap.Mpc_over_h
-thickness = 80.0*snap.Mpc_over_h
+	#Get the positions of the particles
+	snap.getPositions()
 
-#Cut the lenses
-for cut,pos in enumerate(cut_points):
-	for normal in range(3):
+	#Decide where to cut the lenses
+	cut_points = np.array([40.0,120.0,200.0]) * snap.Mpc_over_h
+	thickness = 80.0*snap.Mpc_over_h
 
-		if pool is not None and pool.is_master():
-			print("Cutting plane at {0} with normal {1},thickness {2}, of size {3} x {3}".format(pos,normal,thickness,2.9*deg))
+	#Cut the lenses
+	for cut,pos in enumerate(cut_points):
+		for normal in range(3):
 
-		#Do the cutting
-		plane,resolution,NumPart = snap.cutLens(normal=normal,center=pos,thickness=thickness,left_corner=np.zeros(3)*snap.Mpc_over_h,plane_size=2.9*deg,plane_resolution=plane_resolution,thickness_resolution=1,smooth=2,kind="potential")
+			if pool is not None and pool.is_master():
+				print("Cutting plane at {0} with normal {1},thickness {2}, of size {3} x {3}".format(pos,normal,thickness,2.9*deg))
 
-		if pool is None or pool.is_master():
+			#Do the cutting
+			plane,resolution,NumPart = snap.cutLens(normal=normal,center=pos,thickness=thickness,left_corner=np.zeros(3)*snap.Mpc_over_h,plane_size=2.9*deg,plane_resolution=plane_resolution,thickness_resolution=1,smooth=2,kind="potential")
+
+			if pool is None or pool.is_master():
 			
-			#Wrap the plane in a PotentialPlane object
-			potential_plane = PotentialPlane(plane,angle=2.9*deg,redshift=snap.header["redshift"],cosmology=snap.cosmology,num_particles=NumPart)
+				#Wrap the plane in a PotentialPlane object
+				potential_plane = PotentialPlane(plane,angle=2.9*deg,redshift=snap.header["redshift"],cosmology=snap.cosmology,num_particles=NumPart)
 
-			#Save the result
-			plane_file = os.path.join(save_path,"snap{0}_potentialPlane{1}_normal{2}.fits".format(comm.rank//16,cut,normal))
-			print("Saving plane to {0}".format(plane_file))
-			potential_plane.save(plane_file)
+				#Save the result
+				plane_file = os.path.join(save_path,"snap{0}_potentialPlane{1}_normal{2}.fits".format(n,cut,normal))
+				print("Saving plane to {0}".format(plane_file))
+				potential_plane.save(plane_file)
 			
 			
-		if pool is not None:
+			if pool is not None:
 			
-			#Safety barrier sync
-			pool.comm.Barrier()
+				#Safety barrier sync
+				pool.comm.Barrier()
 
 
-#Close the snapshot
-snap.close()
+	#Close the snapshot
+	snap.close()
+
+
+if pool is not None and pool.is_master():
+	print("DONE!!")
